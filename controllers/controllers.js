d96ba0aafbb3b56f750759aafff09e8323a7342a
Sagar <sagar785mishra@gmail.com>
2023-04-30 00:56:46 +0530

    First Commit

---
+++ controllers/controllers.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync(`${__dirname}/../models/data.json`));
+
+exports.addEvent = (req,res)=>{
+    const obj = req.body;
+    data.push(obj);
+    fs.writeFile(`${__dirname}/../models/data.json`,JSON.stringify(data),(err)=>{
+        console.log(err);
+    });
+    res.send(obj);
+}
+
+exports.getByID=(req,res)=>{
+    let id=req.params.id;
+    console.log(id,typeof id);
+    const obj = data.find((ele)=>{
+        return ele.uid == id;
+    })
+    res.send(obj);
+}
+
+exports.deletebyID=(req,res)=>{
+    let id=req.params.id;
+    const idx = data.findIndex((ele)=>{
+        return ele.uid == id;
+    })
+    data.splice(idx,1);
+    res.json({
+        status:"Deleted"
+    })
+
+}
+
+exports.getAll=(req,res)=>{
+    res.send(data);
+}
+
+
+exports.updatebyId=(req,res)=>{
+    let id=req.params.id;
+    let body=req.body;
+    console.log(typeof body,body);
+    const idx = data.findIndex((ele)=>{
+        return ele.uid == id;
+    })
+    data[idx]=body;
+    fs.writeFile(`${__dirname}/../data.json`,JSON.stringify(data),(err)=>{
+        console.log(err);
+    });
+    res.send("Data updated");
+
+}
+
---
+++ routes/route.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const { addEvent, getByID, getAll, deletebyID, updatebyId } = require('../controllers/controllers');
+
+
+
+const router = express.Router();
+
+router.route('/events/:id').get(getByID).put(updatebyId).delete(deletebyID);
+router.route('/events').post(addEvent).get(getAll);
+
+module.exports ={
+    router
+}
